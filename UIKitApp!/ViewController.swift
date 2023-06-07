30d66f0012255f131702f82fbee79bc1fa2a124d
Susanna <susanna1235@gmail.com>
2023-06-07 16:36:43 +0300

    I created a button and label combined their actions, so that when a specific button is selected, the text and text color change.

--- UIKitApp!/ViewController.swift
+++ UIKitApp!/ViewController.swift
@@ -2,18 +2,49 @@
 //  ViewController.swift
 //  UIKitApp!
 //
-//  Created by Susanna Zakaryan on 06.06.2023.
+//  Created by Susanna R on 06.06.2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var segmentedControll: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    
+    
+    //MARK: Segmented Controll
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        segmentedControll.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        //MARK: Label
+        
+        mainLabel.text = "" //изначальный текст
+        mainLabel.font.withSize(36) //размер шрифта
+        mainLabel.textAlignment = .center //текст посередке
+        // mainLabel.numberOfLines = 0 если написать так то строчек будет бесконечно кол-во
+        mainLabel.numberOfLines =  2
     }
-
-
+    
+    
+    
+    @IBAction func segmentedControllAction(_ sender: Any) {
+        
+        switch segmentedControll.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "выбран с индексом 0"
+            mainLabel.textColor = .brown
+        case 1:
+            mainLabel.text = "выбран с индексом 1"
+            mainLabel.textColor = .green
+        case 2:
+            mainLabel.text = "выбран с индексом 2"
+            mainLabel.textColor = .red
+        default: break
+        }
+        
+    }
+    
 }
-
